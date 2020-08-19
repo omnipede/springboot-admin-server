ec024ad536b99d9071843b03bf8fcc387d31d9b0
서현규 <omnipede@coinplug.com>
2020-08-19 16:45:10 +0900

    기능: Spring security 적용

---
+++ src/main/java/io/omnipede/springbootadminserver/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package io.omnipede.springbootadminserver;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final AdminServerProperties adminServer;
+
+    public SecurityConfiguration(AdminServerProperties adminServer) {
+        this.adminServer = adminServer;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
+
+        http.authorizeRequests()
+                .antMatchers(this.adminServer.path("/assets/**")).permitAll()
+                .antMatchers(this.adminServer.path("/login")).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(this.adminServer.path("/login")).successHandler(successHandler).and()
+                .logout().logoutUrl(this.adminServer.path("/logout")).and()
+                .httpBasic().and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(
+                        this.adminServer.path("/instances"),
+                        this.adminServer.path("/actuator/**")
+                );
+
+    }
+}
